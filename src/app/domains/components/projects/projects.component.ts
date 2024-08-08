df6205f8844faf572521aadfcbd5f1d0234c2a0d
Luis Angel <neal.ocaffry@gmail.com>
2024-08-08 17:43:27 -0600

    cambios

--- src/app/domains/components/projects/projects.component.ts
+++ src/app/domains/components/projects/projects.component.ts
@@ -15,9 +15,17 @@ export class ProjectsComponent {
     {
       titulo : 'Programación',
       subtitulo : 'Desarrollo de Soluciones Digitales Personalizadas',
-      description : 'Entiendo que cada negocio es único, y me dedico a desarrollar aplicaciones a la medida que se adaptan perfectamente a tus necesidades y objetivos. Trabajemos juntos para transformar tus ideas en soluciones digitales innovadoras y eficientes. Con un enfoque colaborativo y una pasión por la tecnología, estoy aquí para asegurarme de que cada paso del camino sea fluido y exitoso. ¡Hablemos sobre cómo podemos hacer crecer tu negocio con tecnología a medida!',
+      description : 'Cada negocio es único. Desarrollo aplicaciones a medida para adaptarse a tus necesidades y objetivos. Transformemos tus ideas en soluciones digitales innovadoras y eficientes. Hablemos sobre cómo crecer tu negocio con tecnología personalizada.',
       enterprise : '',
-      image : "../../../../assets/images/programing.jpg",
+      image : "../../../../assets/images/Desarrollo.jpg",
+      image_descrip : ''
+    },
+    {
+      titulo : 'Tareas y trabajos',
+      subtitulo : 'Gestión Eficiente de Tareas y Proyectos con Microsoft Office y Otras Herramientas Digitales',
+      description : 'Ofrezco servicios personalizados para la gestión y creación de documentos en Microsoft Office, así como en otras herramientas digitales. Desde tareas escolares hasta proyectos profesionales, garantizo una organización eficiente y resultados de alta calidad para satisfacer todas tus necesidades.',
+      enterprise : '',
+      image : "../../../../assets/images/trabajos_investigacion.jpg",
       image_descrip : ''
     },
     {
@@ -25,7 +33,7 @@ export class ProjectsComponent {
       subtitulo : 'Desarrollo de Soluciones Digitales Personalizadas',
       description : 'Entiendo que cada negocio es único, y me dedico a desarrollar aplicaciones a la medida que se adaptan perfectamente a tus necesidades y objetivos. Trabajemos juntos para transformar tus ideas en soluciones digitales innovadoras y eficientes. Con un enfoque colaborativo y una pasión por la tecnología, estoy aquí para asegurarme de que cada paso del camino sea fluido y exitoso. ¡Hablemos sobre cómo podemos hacer crecer tu negocio con tecnología a medida!',
       enterprise : '',
-      image : "../../../../assets/images/programing.jpg",
+      image : "../../../../assets/images/AnalisisDatos.jpg",
       image_descrip : ''
     }
   ]
