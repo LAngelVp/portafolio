97e88ab6c9cc19c7bd8427a97c3ab47bdfa6189c
Luis Angel <neal.ocaffry@gmail.com>
2025-06-23 15:59:58 -0600

    migracion 18

--- src/app/domains/components/contact-me/contact-me.component.ts
+++ src/app/domains/components/contact-me/contact-me.component.ts
@@ -7,7 +7,11 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    HttpClientModule
+    
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule
   ],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.css'
